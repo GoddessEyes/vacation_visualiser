a3eb3c16ad494241eeeb8bf01137970022035323
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-01-13 07:03:33 +1000

    Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python src/manage.py migrate && python src/manage.py runserver
--- src/vacation_visualiser/settings/dev.py
+++ src/vacation_visualiser/settings/dev.py
@@ -1,6 +1,14 @@
 """Dev настройки приложения."""
+import dj_database_url
 
 from .base import *
+import os
 
+DATABASE_URL = os.getenv('DATABASE_URL', False)
+
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASE_URL
+    )
 
 DEBUG = True

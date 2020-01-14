fa88b5b3047c776930ba960e72c3c6109fa29c5d
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-01-15 09:25:29 +1000

    HEROKU

--- src/vacation_visualiser/wsgi.py
+++ src/vacation_visualiser/wsgi.py
@@ -5,6 +5,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vacation_visualiser.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.vacation_visualiser.settings.dev')
 
 application = get_wsgi_application()

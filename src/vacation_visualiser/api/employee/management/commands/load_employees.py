a5f01408adacddd2464ca9441fed56de980c07bd
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-01-16 00:44:20 +1000

    Fix project

--- src/vacation_visualiser/api/employee/management/commands/load_employees.py
+++ src/vacation_visualiser/api/employee/management/commands/load_employees.py
@@ -1,57 +1,65 @@
+"""Команда загрузит Сотрудников в модель `Employee` из xlsx файла.
+
+| Фамилия Имя Отчество |
+| Фамилия Имя Отчество |
+"""
+
+import logging
+import os
+from typing import Dict, List
+
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
 from django.utils.text import slugify
 from openpyxl import load_workbook
-import os
-from vacation_visualiser.api.employee.models import Employee
 from transliterate import translit
-import logging
+from vacation_visualiser.api.employee.models import Employee
+
+
+logging.basicConfig(
+    filename='import_employees_log.txt',
+    level=logging.INFO,
+)
 
 
 class Command(BaseCommand):
+    """Команда работает только с файлом employees.xlsx. FIX."""
 
-    logging.basicConfig(
-        filename='import_employees_log.txt',
-        level=logging.INFO,
-    )
-
-    def handle(self, *args, **options):
+    def handle(self, *args: List[str], **options: Dict[str, str]):
+        """Обработчик xlsx."""
         wb = load_workbook(
-            os.path.dirname(os.path.abspath(__file__)) + '/employees.xlsx'
+            f'{os.path.dirname(os.path.abspath(__file__))}/employees.xlsx',
         ).active
 
         for item in (item[0].value for item in wb.iter_rows()):
             try:
                 last_name, first_name, middle_name = item.split()
-            except AttributeError as err:
-                logging.info(
-                    f'Ошибка импорта: {item}, возможно пустая строка.\n'
-                    f'Текст ошибки: {err} \n'
-                )
-                continue
-            except ValueError as err:
-                logging.info(
-                    f'Ошибка импорта: {item}, возможно не полное ФИО.\n'
-                    f'Текст ошибки: {err} \n'
-                )
-                continue
-            try:
-                Employee(
-                    username=slugify(
-                        translit(
-                            f'{last_name} {first_name}',
-                            'ru',
-                            reversed=True,
-                        )
+                username = slugify(
+                    translit(
+                        f'{last_name} {first_name}', 'ru', reversed=True,
                     ),
+                )
+                Employee(
+                    username=username,
                     first_name=first_name,
                     last_name=last_name,
                     middle_name=middle_name,
                     password=last_name,
                 ).save()
+            except AttributeError as err:
+                logging.info((
+                    f'Ошибка импорта: {item}, возможно пустая строка.\n'
+                    f'Текст ошибки: {err} \n'
+                ))
+            except ValueError as err:
+                logging.info((
+                    f'Ошибка импорта: {item}, возможно не полное ФИО.\n'
+                    f'Текст ошибки: {err} \n'
+                ))
+
             except IntegrityError as err:
-                logging.info(
+                logging.info((
                     f'Ошибка импорта: {last_name, first_name, middle_name}. \n'
                     f'Возможно сотрудник уже существует. \n'
                     f'Текст ошибки: {err} \n'
-                )
+                ))
--- src/vacation_visualiser/api/vacation/import_serializers.py
+++ src/vacation_visualiser/api/vacation/import_serializers.py
@@ -1,11 +1,14 @@
+"""Serializer`s для внешних `app`. FIX: cross-import."""
+
 from typing import Tuple
 
 from rest_framework import serializers
-
 from vacation_visualiser.api.vacation.models import Vacation
 
 
 class UserVacationsListSerializer(serializers.ModelSerializer):
+    """Serializer для последовательности `Отпуска Юзера`."""
+
     class Meta:
         model: Vacation = Vacation
         fields: Tuple[str, ...] = (

02e39c29a0ad85b16517499f636685569794eb98
igor shcherbakov <igor.shcherbakov@fogstream.ru>
2020-01-16 00:44:20 +1000

    Fix project

--- src/vacation_visualiser/api/employee/views.py
+++ src/vacation_visualiser/api/employee/views.py
@@ -1,48 +1,55 @@
 """View`s модуля `Сотрудники`."""
 
+from typing import List, Tuple
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from vacation_visualiser.api.employee.models import (
-    Employee, Position, Department
+    Department,
+    Employee,
+    Position,
 )
 from vacation_visualiser.api.employee.serializers import (
+    DepartmentSerializer,
     EmployeeListSerializer,
     EmployeeRetrieveSerializer,
     PositionSerializer,
-    DepartmentSerializer,
 )
-from typing import List, Tuple
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
 
 
 class EmployeeView(ReadOnlyModelViewSet):
     """View API `Сотрудники`."""
 
-    queryset: 'List[Employee]' = Employee.objects.all()
-    permission_classes: 'Tuple[object, ...]' = (IsAuthenticated, )
-    filter_backends: Tuple[object, ...] = (DjangoFilterBackend, SearchFilter, )
-    filterset_fields = ('position', )
-    search_fields = ('first_name', 'middle_name', 'last_name', 'position__name')
+    queryset: List[Employee] = Employee.objects.all()
+    permission_classes: Tuple[object, ...] = (IsAuthenticated, )
+    filter_backends: Tuple[object, ...] = (DjangoFilterBackend, SearchFilter)
+    filterset_fields: Tuple[str, ...] = ('position', )
+    search_fields: Tuple[str, ...] = (
+        'first_name', 'middle_name', 'last_name', 'position__name',
+    )
 
     def get_serializer_class(self):
+        """Возвращает разные serializer`s для list/retrieve запросов."""
         if self.action == 'retrieve':
             return EmployeeRetrieveSerializer
-        else:
-            return EmployeeListSerializer
+        return EmployeeListSerializer
 
 
 class PositionView(ReadOnlyModelViewSet):
     """View API `Должность`."""
-    queryset: 'List[Position]' = Position.objects.all()
-    permission_classes: 'Tuple[object, ...]' = (IsAuthenticated, )
+
+    queryset: List[Position] = Position.objects.all()
+    permission_classes: Tuple[object, ...] = (IsAuthenticated, )
     serializer_class = PositionSerializer
     filter_backends: Tuple[object, ...] = (DjangoFilterBackend, )
-    filterset_fields = ('name', 'department', )
+    filterset_fields = ('name', 'department')
 
 
 class DepartmentView(ReadOnlyModelViewSet):
     """View API `Отдел`."""
+
     queryset: 'List[Department]' = Department.objects.all()
     permission_classes: 'Tuple[object, ...]' = (IsAuthenticated, )
     serializer_class = DepartmentSerializer
